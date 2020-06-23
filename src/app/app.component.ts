a6f8b5f5ea04f11f185902de4e4a79ba96b36f08
KadoTomohiro <corner0427@gmail.com>
2020-06-24 02:27:27 +0900

    add cache system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,114 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Injectable, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'bindTest';
+export class AppComponent implements OnDestroy{
+  store: Store;
+  key: string;
+  constructor(private cacheService: CacheService)  {
+    this.store = new Store();
+    this.key = this.cacheService.bind(this.store);
+    this.cacheService.restore(this.key)
+      .then(cacheData => {
+        if (cacheData) {
+          this.store = {...cacheData};
+        }
+      });
+  }
+
+  @HostListener('window:beforeunload', [ '$event' ])
+  caching() {
+    this.cacheService.store(this.key);
+  }
+
+  ngOnDestroy() {
+    this.caching();
+  }
+}
+
+class Store {
+  title: string;
+  detail: string;
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+class CacheService {
+
+  cacheStore: { [key: string]: Cache };
+
+  constructor(private router: Router, private cacheRepository: CacheRepository) {
+    this.cacheStore = {};
+  }
+
+  bind(store: any, keyGenerator: KeyGenerator = new UrlContextKeyGenerator(this.router.url)): string {
+    const newCache = new Cache(store, keyGenerator);
+    const key = newCache.key;
+    this.cacheStore[key] = newCache;
+    return key;
+  }
+
+  store(key: string): Promise<any> {
+    const cache = this.getCache(key);
+    return this.cacheRepository.store(cache);
+  }
+  restore(key: string): Promise<any> {
+    return this.cacheRepository.restore(key);
+  }
+
+  private getCache(key: string) {
+    return this.cacheStore[key];
+  }
+}
+
+
+
+class Cache {
+  readonly key: string;
+  constructor(public store: any, private keyGenerator: KeyGenerator) {
+    this.key = this.keyGenerator.generate();
+  }
+}
+
+class UrlContextKeyGenerator implements KeyGenerator {
+  constructor(private url: string) {
+  }
+
+  generate(): string {
+    return JSON.stringify({url: this.url});
+  }
+}
+
+interface KeyGenerator {
+  generate(): string;
+}
+@Injectable()
+export abstract class CacheRepository {
+  abstract store(cache: Cache): Promise<any>;
+  abstract restore(key: string): Promise<any | null>;
+}
+
+export class LocalStorageCacheRepository implements CacheRepository {
+  restore(key: string): Promise<any | null> {
+    return new Promise<any|null>((resolve => {
+      const rowData = localStorage.getItem(key);
+      const result = JSON.parse(rowData);
+      resolve(result);
+    }));
+  }
+
+  store(cache: Cache): Promise<any> {
+    return new Promise<any>(resolve => {
+      const storeData = JSON.stringify(cache.store);
+      localStorage.setItem(cache.key, storeData);
+      resolve();
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent, CacheRepository, LocalStorageCacheRepository } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: CacheRepository, useClass: LocalStorageCacheRepository}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
